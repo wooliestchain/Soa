e045ef450107207735a50b0617e3c63b6c080f45
Emmanuel <levyren38@gmail.com>
2023-04-02 01:10:41 +0100

    .

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#Réalisé par Emmanuel Levy MBINA
+#Une API REST pour une plateforme de gestion de d'activité commerciale .
+# Le projet est conçu à l'aide du framework flask de python ,
+# cet API est conçu comme un web service qui va permettre à différentes applications de pouvoir faire des appels.
+# Nous avons défini deux classes , la classe produit et la classe vente.
+
+#A REST API for a business activity management platform.
+# The project is designed using the python flask framework,
+# this API is designed as a web service that will allow different applications to be able to make calls.
+# We have defined two classes, the product class and the sale class.
--- product.py
+++ product.py
@@ -3,6 +3,17 @@ import sqlite3
 
 app = Flask(__name__)
 
+#Réalisé par Emmanuel Levy MBINA
+#Une API REST pour une plateforme de gestion de d'activité commerciale .
+# Le projet est conçu à l'aide du framework flask de python ,
+# cet API est conçu comme un web service qui va permettre à différentes applications de pouvoir faire des appels.
+# Nous avons défini deux classes , la classe produit et la classe vente.
+
+#A REST API for a business activity management platform.
+# The project is designed using the python flask framework,
+# this API is designed as a web service that will allow different applications to be able to make calls.
+# We have defined two classes, the product class and the sale class.
+
 
 class Product:
     def __init__(self, product_name: str, product_categorie: str, product_prix: float):
